716c7df6bd6032837708c1b6216dbfefa4d3767f
sanjanathammayya <53755440+sanjanathammayya@users.noreply.github.com>
2019-09-09 16:57:32 +0530

    Add files via upload

---
+++ sam1/sam1.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+	int k,n,i=0,a[100],s=0,d=0,t=1,c=0;
+	scanf("%d",&t);
+	if(t<10)
+	{
+		while(c<t)
+		{
+			printf("no of elements");
+			scanf("%d",&n);
+			printf("enter the kth element"):
+				scanf("%d",&k);
+			printf("enter the array");
+			for(i=0;i<n;i++)
+				sacnf("%d",&a);
+			s=a[0];
+			for(i=0;i<n;i++)
+			{
+				if(a[i]<s)
+				{
+					s=a[i];
+				}
+			}
+			if(s==k)
+			{
+				printf("%d",d);
+			}
+			else
+			{
+				while(s<k)
+				{
+					s++;
+					d++;
+					printf("%d",d);
+					s=0;
+					  d=0;
+					  c++;
+				}
+			}
+		}
+
+                E492: Not an editor command: wk                        
